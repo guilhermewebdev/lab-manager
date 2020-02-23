1ca30ca9bb8044b8ebf31ee5de07fb0f75918066
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-02-22 23:38:15 -0300

    instalação do rest framework

--- lab-manager/server/api/api/settings.py
+++ lab-manager/server/api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'labs.apps.LabsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
