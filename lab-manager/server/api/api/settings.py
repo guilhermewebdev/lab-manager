9570cf16dfa08c57441e6c24e2cc0361534c622d
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-02-23 00:46:19 -0300

    substituição do rest fremework pelo graphene

--- lab-manager/server/api/api/settings.py
+++ lab-manager/server/api/api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'labs.apps.LabsConfig',
-    'rest_framework',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -70,14 +70,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'api.wsgi.application'
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
