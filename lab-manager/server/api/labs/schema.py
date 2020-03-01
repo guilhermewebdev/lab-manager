55701931cd6f55ba73bcba3b8532d28d0aede744
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-01 02:51:13 -0300

    mutação do profissional

--- lab-manager/server/api/labs/schema.py
+++ lab-manager/server/api/labs/schema.py
@@ -23,6 +23,23 @@ class ProfessionalType(types.DjangoObjectType):
             'roles',
         )
 
+class ProfessionalMutation(mutation.DjangoFormMutation):
+    professional = graphene.Field(ProfessionalType)
+
+    @classmethod
+    def perform_mutate(cls, form, info):
+        lab = get_lab(info.context.user.labs, form.cleaned_data.pop('lab'))
+        user = None
+        if 'id' in form.cleaned_data and 'password' not in form.cleaned_data:
+            user = models.Professional.objects.update(**form.cleaned_data)
+        elif 'password' in form.cleaned_data:
+            user = models.Professional.objects.create_user(**form.cleaned_data)
+        if user: user.labs.add(lab)
+        return cls(professional=user)
+
+    class Meta:
+        form_class = forms.ProfessionalUpdateForm
+
 class RoleType(types.DjangoObjectType):
 
     class Meta:
@@ -42,16 +59,7 @@ class RoleMutation(mutation.DjangoFormMutation):
         role = None
         lab = get_lab(info.context.user.labs, form.cleaned_data.pop('lab'))
         if 'permissions' in form.cleaned_data:
-            permissions = form.cleaned_data.pop('permissions')
-        if 'id' in form.cleaned_data:
-            role = models.Role.objects.filter(
-                id=form.cleaned_data.pop('id'),
-                lab=lab
-            ).update(**form.cleaned_data)
-        else: role = models.Role(
-                **form.cleaned_data,
-                lab=lab,
-            )
+            permissions = form.cleaned_data.pop('permissions')        
         role.save()
         if permissions != []:
             role.permissions.clear()
