b00d6a8eeb18271c001fc9d07a9d965f8d8b2eb6
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-14 14:02:53 -0300

    verificação na middleware

--- lab-manager/server/api/api/middlewares.py
+++ lab-manager/server/api/api/middlewares.py
@@ -8,7 +8,7 @@ def remove_server_header(get_response):
 
 def set_laboratory(next, root, info, **args):
     if 'input' in args:
-        if 'lab' in args['input']:
+        if ('lab' in args['input']) and info.context.user.is_authenticated:
             labs = list(info.context.user.labs.all().iterator())
             args['input'].update({
                 'lab': labs[max(min(int(args['input'].pop('lab')), len(labs)), 0)]
