28bd8591cb64dc93592ea0dff71fbf9b286d2dde
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-16 23:53:26 -0300

    resolução do process e procedure dos stages

--- lab-manager/server/api/jobs/schemas/stage.py
+++ lab-manager/server/api/jobs/schemas/stage.py
@@ -1,8 +1,17 @@
 from graphene_django import types
 import graphene
+from . import procedure, process
 from jobs import models
 
 class StageType(types.DjangoObjectType):
+    procedure = graphene.Field(procedure.ProcedureType)
+    process = graphene.Field(process.ProcessType)
+
+    def resolve_procedure(parent, info, **kwargs):
+        return parent.procedure
+
+    def resolve_process(parent, info, **kwargs):
+        return parent.process
 
     class Meta:
         model = models.Stage
