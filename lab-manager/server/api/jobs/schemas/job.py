dde9a8dffba537774cb33a5a03fdcfcece2cbbe3
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-21 23:52:29 -0300

    criação da query jobs

--- lab-manager/server/api/jobs/schemas/job.py
+++ lab-manager/server/api/jobs/schemas/job.py
@@ -19,3 +19,18 @@ class JobType(types.DjangoObjectType):
         'delivered',
         'deadline',
     )
+
+class JobQuery:
+    jobs = graphene.List(JobType)
+    job = graphene.Field(
+        JobType,
+        index=graphene.Int(required=True),
+        lab=graphene.Int(required=True),
+    )
+
+    def resolve_jobs(parent, info, **kwargs):
+        return parent.jobs.filter(**kwargs).all().iterator()
+
+    def resolve_job(parent, info, **kwargs):
+        return parent.jobs.get(**kwargs)
+
