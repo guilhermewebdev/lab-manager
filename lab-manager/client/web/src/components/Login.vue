f5a92c9e303008e8e80bb3a4fdec4548eacdc366
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-23 01:56:13 -0300

    criação do formulário de login

---
+++ lab-manager/client/web/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-form>
+        <v-container
+            fluid
+        >
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Nome de Usuário"
+                        clearable
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Senha"
+                        clearable
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="show1 ? 'text' : 'password'"
+                        counter
+                        @click:append="show1 = !show1"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+                    <v-switch
+                        label="Manter conectado"
+                        class="mt-0"
+                    ></v-switch>
+            <v-row>
+                <v-col>
+                    <v-btn
+                        block
+                        color="primary"
+                    >Entrar</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data(){
+        return {
+            show1: false,
+        }
+    }  
+})
+</script>
--- lab-manager/client/web/src/views/Register.vue
+++ lab-manager/client/web/src/views/Register.vue
@@ -12,13 +12,21 @@
                     lg="5"
                     xl="4"
                 >
-                    <v-card>
-                        <v-tabs
-                            grow
+                    <v-card
+                        shaped
+                        raised
+                    >
+                        <v-card-text>
+                            <v-tabs
+                            grow                            
                         >
                             <v-tab>Entrar</v-tab>
                             <v-tab>Cadastrar</v-tab>
+                            <v-tab-item>
+                                <login></login>
+                            </v-tab-item>
                         </v-tabs>
+                        </v-card-text>                        
                     </v-card>
                 </v-col>
             </v-row>
@@ -27,7 +35,10 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import Login from '@/components/Login.vue'
 export default Vue.extend({
-    
+    components: {
+        Login
+    }
 })
 </script>
