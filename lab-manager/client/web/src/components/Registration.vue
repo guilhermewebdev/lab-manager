063264e071f938eecea5bc93bb42c749a0a672c7
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-29 17:17:20 -0300

    criação do formulário de cadastro

--- lab-manager/client/web/src/components/Registration.vue
+++ lab-manager/client/web/src/components/Registration.vue
@@ -1,9 +1,154 @@
 <template>
-    <v-form></v-form>
+    <v-form
+        ref="form"
+        @keypress.enter.native="submit"
+    >
+        <v-container
+            fluid
+        >
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Nome Completo"
+                        clearable
+                        v-model="data.fullName"
+                        required
+                        :rules="[rules.name, rules.required]"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="E-mail"
+                        clearable
+                        :rules="[rules.email, rules.required]"
+                        v-model="data.email"
+                        required
+                    ></v-text-field>
+                </v-col>
+            </v-row>  
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Nome de Usuário"
+                        clearable
+                        v-model="data.username"
+                        :rules="[rules.required, rules.username]"
+                        required
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Senha"
+                        clearable
+                        v-model="data.password"
+                        :rules="[rules.required, rules.password]"
+                        required
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Confirme sua senha"
+                        clearable
+                        v-model="passwordVerify"
+                        required
+                        :rules="[rules.password, rules.required]"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-text-field
+                        label="Nome do laboratório"
+                        :rules="[rules.required, rules.name]"
+                        clearable
+                        v-model="data.lab"
+                        required
+                    ></v-text-field>
+                </v-col>
+            </v-row> 
+            <v-row>
+                <v-col>
+                    <v-checkbox
+                        required
+                        v-model="checkbox"
+                        :rules="[rules.required]"
+                        class="mt-0"
+                    >
+                        <template v-slot:label>
+                            <div>
+                            Eu concordo com
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                    <a
+                                        target="_blank"
+                                        href="http://vuetifyjs.com"
+                                        @click.stop
+                                        v-on="on"
+                                    >
+                                        os termos de uso
+                                    </a>
+                                </template>
+                                Abrir em nova aba
+                            </v-tooltip>
+                            do aplicativo
+                            </div>
+                        </template>
+                    </v-checkbox>
+                </v-col>
+            </v-row>  
+            <v-row>
+                <v-col>
+                    <v-btn
+                        @click.stop="submit"
+                        block
+                        color="primary"
+                        :loading="loading"
+                    >Cadastrar</v-btn>
+                </v-col>
+            </v-row>            
+        </v-container>
+    </v-form>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    name: "registration"
+    name: "registration",
+    data(){
+        return {
+            loading: false,
+            passwordVerify: null,
+            data: {
+                fullName: null,
+                username: null,
+                email: null,
+                password: null,
+                lab: null,                
+            },
+        }
+    },
+    computed: {
+        rules(){
+            return {
+                required: v => !!v || "Este campo é obrigatório",
+                name: v => /((-?[A-z])+[A-Za-z]+[ ]?)$/igm.test(v) || "Digite um nome válido",
+                email: v => /^([a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$/igm.test(v) || 'Digite um email válido',
+                username: v => /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm.test(v) || "Nome de usuário inválido",
+                password: v => this.data.password === this.passwordVerify || "As senhas não coincidem"
+            }
+        }
+    },
+    methods: {
+        submit(){
+            if(this.$refs.form.validate()){
+                alert()
+            }
+        }
+    }
 })
 </script>
