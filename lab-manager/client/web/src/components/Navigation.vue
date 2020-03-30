75af3554ea171aed5a89ebb37e0d5d56dbe013a9
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-03-30 04:28:07 -0300

    implementação do dark mode

--- lab-manager/client/web/src/components/Navigation.vue
+++ lab-manager/client/web/src/components/Navigation.vue
@@ -3,8 +3,17 @@
     <v-list>
             <v-list-item class="px-2">
               <v-list-item-avatar>
-                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                <v-avatar color="teal" size="48">
+                    <span class="white--text headline">48</span>
+                </v-avatar>
               </v-list-item-avatar>
+            <v-spacer></v-spacer>
+            <v-btn
+                icon
+                @click.stop="dark = !dark"
+            >
+                <v-icon>{{ dark ? 'mdi-weather-sunny' : 'mdi-moon-waning-crescent'  }}</v-icon>
+            </v-btn>
             </v-list-item>
 
             <v-list-item link>
@@ -48,10 +57,13 @@ export default Vue.extend({
     props: {
         drawer: Boolean,
     },
+    data: () => ({
+        dark: Boolean(localStorage.getItem('dark')),
+    }),
     watch: {
-        drawer(newV, oldV){
-            alert(newV)
-            alert(oldV)
+        dark(newValue){
+            localStorage.setItem('dark', newValue)
+            this.$vuetify.theme.dark = newValue;
         }
     }
 })
--- lab-manager/client/web/src/plugins/vuetify.ts
+++ lab-manager/client/web/src/plugins/vuetify.ts
@@ -30,6 +30,7 @@ export default new Vuetify({
         warning: '#FFC107'
       }
     },
+    dark: Boolean(localStorage.getItem('dark'))
   },
     lang: {
       locales: { pt },
