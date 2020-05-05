ad86d6849ac3334db901e352168e1fcb2da657c7
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-05 02:38:26 -0300

    Implementação do react

--- lab-manager/client/web/src/app/home/home.module.ts
+++ lab-manager/client/web/src/app/home/home.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { AuthComponent } from './auth/auth.component';
-
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [HomeComponent, AuthComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatTabsModule
   ]
 })
 export class HomeModule { }
