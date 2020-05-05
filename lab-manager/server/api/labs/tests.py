ba6d4be6127d18e4333002f804cd49a33dc3d67f
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-04 21:35:21 -0300

    Testes de registro graphql

--- lab-manager/server/api/labs/tests.py
+++ lab-manager/server/api/labs/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 import json
 from . import models
+from api.testecase import MyTestCase
 
 class RegistrationTestCase(TestCase):
 
@@ -22,3 +23,51 @@ class RegistrationTestCase(TestCase):
         professional.save()
 
         self.assertIsNotNone(professional.id)
+
+class RegistrationGraphQLTestCase(MyTestCase):
+
+    def test_registration(self):
+        variables = {
+            'fullName': 'Teste da Silva',
+            'username': 'teste',
+            'email': 'teste@teste.com',
+            'password': 'pass534623',
+            'laboratory': 'Teste'
+        }
+        executed = self.client.execute('''
+            mutation registration(
+                $fullName: String!
+                $username: String!
+                $email: String!
+                $password: String!
+                $laboratory: String!
+            ){
+                register(input: {
+                    fullName: $fullName
+                    username: $username
+                    email: $email
+                    password: $password
+                    laboratory: $laboratory
+                }) {
+                    professional {
+                        username
+                    }
+                    laboratory {
+                        name
+                    }
+                }
+            }
+        ''', variables=variables)
+
+        assert executed == {
+            'data': {
+                'register': {
+                    'professional': {
+                        'username': variables['username']
+                    },
+                    'laboratory': {
+                        'name': variables['laboratory']
+                    },
+                }
+            }
+        }
