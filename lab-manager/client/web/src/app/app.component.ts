6a2fcf9d33a9d7c612fab882ef1e2dddd298fae1
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-05 03:10:26 -0300

    Reestruturação do app

--- lab-manager/client/web/src/app/app.component.ts
+++ lab-manager/client/web/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './app.react';
+
+const containerElementName = 'myReactComponentContainer';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `<div #${containerElementName}></div>`,
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+
+export class AppComponent implements OnChanges, OnDestroy, AfterViewInit {
   title = 'web';
+  @ViewChild(containerElementName, {static: false}) containerRef: ElementRef;
+
+  @Input() public counter = 10;
+  @Output() public componentClick = new EventEmitter<void>();
+
+  constructor() {
+    this.handleDivClicked = this.handleDivClicked.bind(this);
+  }
+
+  public handleDivClicked() {
+    if (this.componentClick) {
+      this.componentClick.emit();
+      this.render();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.render();
+  }
+
+  ngAfterViewInit() {
+    this.render();
+  }
+
+  ngOnDestroy() {
+    ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
+  }
+
+  private render() {
+    ReactDOM.render(React.createElement(App), this.containerRef.nativeElement);
+  }
+
 }
--- lab-manager/client/web/src/app/app.react.tsx
+++ lab-manager/client/web/src/app/app.react.tsx
@@ -1,72 +1,12 @@
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    EventEmitter,
-    Input,
-    OnChanges,
-    OnDestroy,
-    Output,
-    SimpleChanges,
-    ViewChild,
-    ViewEncapsulation
-  } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button'
 
-  import * as React from 'react';
-  import * as ReactDOM from 'react-dom';
-  import Button from '@material-ui/core/Button'
-
-const containerElementName = 'myReactComponentContainer';
-
-function App() {
+  
+export function App() {
     return (
-      <Button variant="contained" color="primary">
-        Olá Mundo
-      </Button>
+        <Button variant="contained" color="primary">
+            Olá Mundo
+        </Button>
     );
-  }
-  
-
-@Component({
-  selector: 'app-root',
-  styleUrls: ['./app.component.scss'],
-  template: `<div #${containerElementName}></div>`,
-  encapsulation: ViewEncapsulation.None,
-})
-export class AppComponent implements OnChanges, OnDestroy, AfterViewInit {
-  title = 'web';
-  @ViewChild(containerElementName, {static: false}) containerRef: ElementRef;
-
-  @Input() public counter = 10;
-  @Output() public componentClick = new EventEmitter<void>();
-
-  constructor() {
-    this.handleDivClicked = this.handleDivClicked.bind(this);
-  }
-
-  public handleDivClicked() {
-    if (this.componentClick) {
-      this.componentClick.emit();
-      this.render();
-    }
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.render();
-  }
-
-  ngAfterViewInit() {
-    this.render();
-  }
-
-  ngOnDestroy() {
-    ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
-  }
-
-  
-
-  private render() {
-    ReactDOM.render(<App />, this.containerRef.nativeElement);
-  }
-
 }
