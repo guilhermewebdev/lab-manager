c43e6de5853a217959c392827e15279085c9d953
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-06 22:40:10 -0300

    Validação do formulário de login

--- lab-manager/client/web/src/views/home/Auth.tsx
+++ lab-manager/client/web/src/views/home/Auth.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { useForm } from 'react-hook-form';
+
+import clsx from 'clsx'
+
 import {
     createStyles,
     makeStyles,
@@ -17,8 +21,19 @@ import {
     Switch,
     Button,
     FormControlLabel,
+    FormControl,
+    InputLabel,
+    Input,
     Checkbox,
+    InputAdornment,
+    IconButton,
+    FormHelperText,
 } from '@material-ui/core'
+
+import {
+    Visibility,
+    VisibilityOff,
+} from '@material-ui/icons'
 
 import SwipeableViews from 'react-swipeable-views';
 
@@ -47,7 +62,16 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         button: {
             width: "100%"
-        }
+        },
+        margin: {
+            margin: theme.spacing(1),
+        },
+        withoutLabel: {
+            marginTop: theme.spacing(3),
+        },
+        textField: {
+            width: '25ch',
+        },
     }),
 );
 
@@ -58,6 +82,11 @@ interface TabPanelProps {
     dir?: string;
 }
 
+type Login = {
+    showPassword: boolean,
+    username: string,
+    password: string,
+}
 
 function TabPanel(props: TabPanelProps) {
     const { children, value, index, ...other } = props;
@@ -84,8 +113,30 @@ function a11yProps(index: any) {
 
 function LoginForm() {
     const classes = useStyles();
+    const { register, handleSubmit, watch, errors, reset } = useForm();
+    const onSubmit = (data: any) => console.log(data, reset());
+    const [values, setValues] = React.useState<Login>({
+        username: '',
+        password: '',
+        showPassword: false,
+    });
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+    const handleChange = (prop: keyof Login) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+
     return (
-        <form autoComplete="off" className={classes.form}>
+        <form
+            autoComplete="off"
+            onSubmit={handleSubmit(onSubmit)}
+            className={classes.form}
+        >
             <Grid
                 container
                 justify="center"
@@ -93,13 +144,52 @@ function LoginForm() {
             >
                 <Grid item md={11}>
                     <FormGroup>
-                        <TextField className={classes.input} label="Nome de usuário"></TextField>
-                        <TextField className={classes.input} label="Senha"></TextField>
+                        <TextField
+                            error={errors.username}
+                            name="username"
+                            className={classes.input}
+                            label="Nome de usuário"
+                            helperText={errors.username && "Informe um nome de usuário válido"}
+                            inputRef={register({
+                                required: true,
+                                pattern: /^[a-z0-9_-]{2,}$/
+                            })}
+                        />
+                        <FormControl fullWidth>
+                            <InputLabel htmlFor="standard-adornment-password">Senha</InputLabel>
+                            <Input
+                                name="password"
+                                error={errors.password}
+                                id="standard-adornment-password"
+                                type={values.showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                onChange={handleChange('password')}
+                                inputRef={register({
+                                    required: true,
+                                    minLength: 8,
+                                })}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="Mudar visibilidade da senha"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                            {errors.password && 
+                                <FormHelperText error={errors.password}>Informe uma senha válida, com pelo menos 8 dígitos</FormHelperText>
+                            }
+                        </FormControl>
                         <FormControlLabel className={classes.input} control={<Switch color="primary" />} label="Manter Conectado" />
                     </FormGroup>
                 </Grid>
                 <Grid item md={11}>
-                    <Button className={classes.button} variant="contained" color="primary">
+                    <Button className={classes.button} type="submit" variant="contained" color="primary">
                         Entrar
                     </Button>
                 </Grid>
@@ -108,7 +198,7 @@ function LoginForm() {
     )
 }
 
-function RegisterForm(){
+function RegisterForm() {
     const classes = useStyles();
     return (
         <form autoComplete="off" className={classes.form}>
@@ -119,12 +209,30 @@ function RegisterForm(){
             >
                 <Grid item md={11}>
                     <FormGroup>
-                        <TextField className={classes.input} label="Nome completo"></TextField>
-                        <TextField className={classes.input} label="Email"></TextField>
-                        <TextField className={classes.input} label="Nome de usuário"></TextField>
-                        <TextField className={classes.input} label="Senha"></TextField>
-                        <TextField className={classes.input} label="Confirme sua senha"></TextField>
-                        <TextField className={classes.input} label="Nome do laboratório"></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Nome completo"
+                        ></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Email"
+                        ></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Nome de usuário"
+                        ></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Senha"
+                        ></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Confirme sua senha"
+                        ></TextField>
+                        <TextField
+                            className={classes.input}
+                            label="Nome do laboratório"
+                        ></TextField>
                         <FormControlLabel className={classes.input} control={<Checkbox color="primary" />} label="Concordo com os termos de uso" />
                     </FormGroup>
                 </Grid>
