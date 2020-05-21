f6253c35fc035682629f53724842ab772ebe0243
guilherme <guilherme@guilhermeweb.dev>
2020-05-21 18:58:14 -0300

    melhoria no comando da api

--- lab-manager/server/api/crm/admin.py
+++ lab-manager/server/api/crm/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from jobs import models as jobs
 
 class TelephonesInline(admin.TabularInline):
     extra = 1
@@ -18,3 +19,19 @@ class ClientAdmin(admin.ModelAdmin):
     list_filter = (
         'lab__name',
     )
+
+
+class JobsInline(admin.StackedInline):
+    extra = 1
+    model = jobs.Job
+
+    def get_fields(self, request, obj=None):
+        if obj:
+            return super(JobsInline, self).get_fields(request, obj)
+        return ('kind', 'amount', 'deadline', 'price', 'description')
+
+@admin.register(models.Patient)
+class PatientAdmint(admin.ModelAdmin):
+    inlines = [
+        JobsInline
+    ]
